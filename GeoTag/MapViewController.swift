bc87d211ff7ad5f8ed5edded7095efff6a1b1bfa
Zahid001 <islam35-1483@diu.edu.bd>
2019-03-28 01:47:50 +0600

    add firebase

--- GeoTag/MapViewController.swift
+++ GeoTag/MapViewController.swift
@@ -9,30 +9,62 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Cosmos
+import Firebase
 
-class MapViewController: UIViewController,UISearchBarDelegate {
 
-    
-    var annotationLocations = [
-        ["title" : "9/A, Ibn Sina trust building, Dhanmondi","lattitude" : 23.7451636, "longitude" : 90.370074 ],
+class MapViewController: UIViewController,UISearchBarDelegate,MKMapViewDelegate {
+
+    var cosmosView = CosmosView()
+    var annotationLocations =
+        [["title" : "9/A, Ibn Sina trust building, Dhanmondi","lattitude" : 23.7451636, "longitude" : 90.370074 ],
         ["title" : "Nilu Squire, Sat Masjid Road, Dhanmondi R/A","lattitude" : 23.7418789, "longitude" : 90.3717531 ]
     
     ]
+    
+    
+    
+    @IBOutlet var popUpView: UIView!
+    @IBOutlet weak var anotationTitle: UITextField!
+    @IBOutlet weak var annotationDescription: UITextField!
+    @IBOutlet weak var ratting: CosmosView!
+    
+    
     
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     let zooming: Double = 1000
     
+    
+    
+    @IBAction func cancelAnnotationInfo(_ sender: Any) {
+        animateOut()
+    }
+    
+    @IBAction func saveAnnotationInfo(_ sender: Any) {
+        ratting.didTouchCosmos = {
+            rating in
+            print("gjhk" ,rating)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        createMultipleAnnotations(locations: annotationLocations)
+        mapView.delegate = self
+        popUpView.layer.cornerRadius = 5
         checkLocationServices()
         //mapView.showsUserLocation = true
-        createMultipleAnnotations(locations: annotationLocations)
-        //showUserZooming()
+        
+        getAnnotaionDetails()
+        
         
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.longPress(_:)))
         self.mapView.addGestureRecognizer(longPressRecognizer)
+        
+        print("hhhhh" , annotationLocations)
     }
     
     func setupLocationManager() {
@@ -77,19 +109,124 @@ class MapViewController: UIViewController,UISearchBarDelegate {
     }
     
     
+    
+    
     @objc func longPress(_ longPressClicked:UILongPressGestureRecognizer)   {
+        
+        let annotationDB = Database.database().reference().child("AnnotationnDetail")
+        var titl:String = ""
+        var textField = UITextField()
+        //var btn = UIButton()
+        
         let location = longPressClicked.location(in: mapView)
+        
         let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
         
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinate
-        annotation.title = "Custom Location"
-        mapView.addAnnotation(annotation)
+        let alert = UIAlertController(title: "Add Location", message: "", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Add Place", style: .default) { (UIAlertAction) in
+
+
+
+                titl = textField.text!
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
+
+                annotation.title = titl
+                let annotationDictionary = [
+                    
+                    "title" : titl,
+                    "lattitude" : coordinate.latitude,
+                    "longitude" : coordinate.longitude,
+                    "star" : 5.0
+                
+                    ] as [String : Any]
+                
+                annotationDB.childByAutoId().setValue(annotationDictionary){
+                    (error,ref) in
+                    
+                    if error != nil {
+                        print(error!)
+                    } else {
+                        print("details saved")
+                    }
+                }
+                print(coordinate.latitude)
+
+                self.annotationLocations.append(annotationDictionary)
+                self.mapView.addAnnotation(annotation)
+
+
+
+            }
+
+            alert.addAction(action)
+
+            alert.addTextField { (UITextField) in
+                UITextField.placeholder = "Give location name"
+                textField = UITextField
+
+            }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction!) in
+            print("Cancel button tapped");
+        }
+        alert.addAction(cancelAction)
+
+
+            present(alert,animated: true)
+            
+        }
+        
+    func getAnnotaionDetails(){
+        let annotationDB = Database.database().reference().child("AnnotationnDetails")
+        annotationDB.observe(.childAdded) { (DataSnapshot) in
+            let annotationValue = DataSnapshot.value as! Dictionary<String , Any>
+            let title = annotationValue["title"]
+            let lat = annotationValue["lattitude"]
+            let lon = annotationValue["longitude"]
+            print(lat!, lon!, title!)
+            let dict = [
+                "title" : title!,
+                "lattitude" : lat!,
+                "longitude" : lon!
+            ] as [String : Any]
+            self.annotationLocations.append(dict  )
+            self.createMultipleAnnotations(locations: self.annotationLocations)
+        }
+    }
+    
+
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        animateIn()
+    }
+    
+    func animateIn() {
+        self.view.addSubview(popUpView)
+        popUpView.center = self.view.center
+        
+        popUpView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        popUpView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            //self.visualEffectView.effect = self.effect
+            self.popUpView.alpha = 1
+            self.popUpView.transform = CGAffineTransform.identity
+        }
         
     }
     
-    
-    
+    func animateOut () {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.popUpView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.popUpView.alpha = 0
+            
+            //self.visualEffectView.effect = nil
+            
+        }) { (success:Bool) in
+            self.popUpView.removeFromSuperview()
+        }
+    }
     
     func createMultipleAnnotations(locations:[[String:Any]])  {
         for location in locations {
